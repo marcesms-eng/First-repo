259b664fad630f180115a10557c1bfbc01d8e852
Marce <marce@Marces-MacBook-Air.local>
2025-09-07 17:42:16 -0300

    Initial commit of first Streamlit app
    :wq#

--- first-app.py
+++ first-app.py
@@ -1,7 +1,15 @@
 import streamlit as st
 
+<<<<<<< HEAD
 st.title("Hello, world! 🌍")
 
 name = st.text_input("¿Cómo te llamas boludo?")
 if name:
     st.write(f"Hola {name}, mono peludo!")
+=======
+st.title("Primera Web App de Marcee! 🌍")
+
+name = st.text_input("¿Cómo te llamas?")
+if name:
+    st.write(f"Hola {name}!")
+>>>>>>> 9cea145 (Initial commit of first Streamlit app)
