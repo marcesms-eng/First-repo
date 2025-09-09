5134751311276e38a923c5599abe51edde38aca0
marcesms-eng <marce.sms@gmail.com>
2025-09-09 22:07:04 +0000

    Agrego tercera app

---
+++ 5app-editar_archivos.py
@@ -0,0 +1,16 @@
+# lectura y escritura de archivos locales
+archivo = "notas.txt"
+
+# Escribir en el archivo
+notas = [4.5, 5.0, 3.8]
+with open(archivo, "w") as f:
+    f.write("Notas registradas:\n")
+    for n in notas:
+        f.write(str(n) + "\n")
+
+# Leer desde el archivo
+with open(archivo, "r") as f:
+    contenido = f.read()
+
+print("Contenido del archivo:")
+print(contenido)
